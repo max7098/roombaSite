30264860c961b1188bbfa78764e3e208c701270a
stevenbrz <sbrz@bu.edu>
2018-01-20 15:55:06 -0500

    this might work

---
+++ roombaServer/server.py
@@ -0,0 +1,32 @@
+from flask import Flask, render_template, request
+import urllib.parse
+import json
+from roomba import *
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    try:
+        if request.method == "POST":
+        
+            attempted_password = request.body['password']
+
+            if attempted_password == "beardbeard":
+                # Process Command Here
+                roomba = Roomba()
+                command = request.body['command']
+                if command == "start":
+                    roomba.start()
+
+                
+            else:
+                # Incorrect Password
+                return "oopsie wrong credentials"
+
+    except Exception as e:
+        return "oopsie what happened here?"
+
+
+if __name__ == "__main__":
+    app.run()
